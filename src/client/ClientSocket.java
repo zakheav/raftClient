e80b56d908e9dedc79b13643abef2cb4f1d40e48
weiyuan <18810543471@163.com>
2017-01-09 15:59:45 +0800

    处理可能出现的粘包问题

--- src/client/ClientSocket.java
+++ src/client/ClientSocket.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import util.Integer_byte_transform;
 
 public class ClientSocket {
 	public Socket socket;
@@ -13,20 +14,33 @@ public class ClientSocket {
 		this.socket = socket;
 	}
 
+	private byte[] wrap(byte[] payload) {// add payload length to the bag
+		byte[] bag = new byte[payload.length + 4];
+		byte[] payloadLength = Integer_byte_transform.intToByteArray(payload.length);
+		bag[0] = payloadLength[0];
+		bag[1] = payloadLength[1];
+		bag[2] = payloadLength[2];
+		bag[3] = payloadLength[3];
+		for (int i = 0; i < payload.length; ++i) {
+			bag[i + 4] = payload[i];
+		}
+		return bag;
+	}
+
 	public void write(String massage) throws IOException {
-		byte[] massageByte = massage.getBytes();
+		byte[] massageByte = wrap(massage.getBytes());
 		OutputStream output = socket.getOutputStream();
 		output.write(massageByte);// block
 		output.flush();
 	}
 
 	public String read() throws IOException, SocketTimeoutException {
-		byte[] buffer = new byte[1024*10];
+		byte[] buffer = new byte[1024 * 10];
 		InputStream input = socket.getInputStream();
-		int length =  input.read(buffer);// block
-		
+		int length = input.read(buffer);// block
+
 		StringBuffer massageBuffer = new StringBuffer(1024 * 10);
-		for (int i = 0; i < length; ++i) {
+		for (int i = 4; i < length; ++i) {
 			massageBuffer.append((char) buffer[i]);
 		}
 		return massageBuffer.toString();
@@ -39,5 +53,5 @@ public class ClientSocket {
 			System.out.println("connection close");
 		}
 	}
-	
+
 }
